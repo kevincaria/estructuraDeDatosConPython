2cc64dfb77077f36175b682412aef443256eebe0
Kevin Caria <kcaria@pigmalion.com.ar>
2023-03-27 18:34:15 -0300

    add primer clase

---
+++ PrimerParcial/00_primer_clase.py
@@ -0,0 +1,19 @@
+'''
+Estructura de datos
+Comisión 2
+Profesor Martin Pustilnik martin.pustilnik@unahur.edu.ar
+
+Ver videos antes de la clase presencial, nos avisan por mail y/o discord
+
+Lunes presencial 18 a 22 lab 7
+Viernes virtual 18 a 22 por meet -> meet.google.com/xiy-avgo-uzg
+
+1er Parcial (15 de Mayo)
+    - Tipos de datos abstractos
+    - Recursividad
+    - Arreglos uni y multidimensionales
+    - Pilas y colas
+
+2do Parcial (3 de Julio)
+Recuperatorios 10 y 14 de Julio
+'''
