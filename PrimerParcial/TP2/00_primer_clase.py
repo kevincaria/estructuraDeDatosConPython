270b0951acd6971796921c002d82e94bce1d0321
Kevin Caria <kcaria@pigmalion.com.ar>
2023-04-07 22:31:24 -0300

    descomentando tp2

--- PrimerParcial/TP2/00_primer_clase.py
+++ PrimerParcial/TP2/00_primer_clase.py
@@ -3,31 +3,31 @@ Ejercicio 1
 Escribir un procedimiento que muestre por pantalla la cadena “Hola mundo!!!” 
 cada vez que se la invoque
 '''
-# def holaMundo():
-#     return print('Hola mundo')
+def holaMundo():
+    return print('Hola mundo')
 
-# holaMundo()
+holaMundo()
 '''
 Ejercicio 2
 Escribir un procedimiento que se le pase por parámetros una cadena 'nombre' y 
 muestre por pantalla: “Hola 'nombre'!!!”
 '''
-# def mostrarPorPantalla(nombre):
-#     return print(f'Hola {nombre} !!')
+def mostrarPorPantalla(nombre):
+    return print(f'Hola {nombre} !!')
 
-# mostrarPorPantalla('Kevin')
+mostrarPorPantalla('Kevin')
 '''
 Ejercicio 3
 Escribir   una   función   que   calcule   y   retorne   el   factorial   de   un   numero   natural.
 '''
 
-# def factorial(n):
-#     result = 1
-#     for i in range(1, n+1):
-#         result = result * i
-#     return result
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result = result * i
+    return result
 
-# print(factorial(3))
+print(factorial(3))
 
 '''
 Ejercicio 4
@@ -36,10 +36,10 @@ debe recibir el importe sin IVA y el porcentaje de IVA a aplicar, y devolver el 
 factura. Si se invoca la función sin pasarle el porcentaje de IVA, deberá aplicar un
 21%.'''
 
-# def calcularImporteConIVA(importe, iva = 21):
-#     return((iva/100)*importe + importe)
+def calcularImporteConIVA(importe, iva = 21):
+    return((iva/100)*importe + importe)
 
-# print(calcularImporteConIVA(100))
+print(calcularImporteConIVA(100))
 
 '''
 Ejercicio 5
@@ -48,25 +48,25 @@ retorna el mayor. Luego, usando esta función, escriba un programa que pida 10 n
 al usuario por teclado y al finalizar informe el mayor por pantalla.
 '''
 
-# def max(numero1, numero2):
+def max(numero1, numero2):
 
-#     if(numero1 > numero2):
-#         resultado = numero1
-#     elif(numero1 < numero2):
-#         resultado = numero2
+    if(numero1 > numero2):
+        resultado = numero1
+    elif(numero1 < numero2):
+        resultado = numero2
 
-#     return resultado
+    return resultado
 
-# def mostrarElMayor():
-#     maximo = int(input('Ingrese el primer numero para tomar como maximo: '))
+def mostrarElMayor():
+    maximo = int(input('Ingrese el primer numero para tomar como maximo: '))
 
-#     for i in range(10):
-#         numeroAComparar = int(input('Ingrese otro numero para comparar: '))
-#         maximo = max(maximo, numeroAComparar)
+    for i in range(10):
+        numeroAComparar = int(input('Ingrese otro numero para comparar: '))
+        maximo = max(maximo, numeroAComparar)
 
-#     return maximo
+    return maximo
 
-# print(mostrarElMayor())
+print(mostrarElMayor())
 
 '''
 Ejercicio 6
@@ -74,15 +74,15 @@ Escribir una función que calcule el área de un círculo y otra que calcule el 
 un cilindro usando la primera función.
 '''
 
-# import math
+import math
 
-# def calcularAreaCirculo(radio):
-#     return math.pi * (radio ** 2)
+def calcularAreaCirculo(radio):
+    return math.pi * (radio ** 2)
 
-# def calcularVolumenCilindro(area, altura):
-#     return area * altura
+def calcularVolumenCilindro(area, altura):
+    return area * altura
 
-# print(calcularVolumenCilindro(calcularAreaCirculo(1),1))
+print(calcularVolumenCilindro(calcularAreaCirculo(1),1))
 
 '''
 Ejercicio 7
@@ -90,15 +90,15 @@ Escribir una función que recibe tres números enteros por parámetros,
 calcula el promedio y retorna “APROBADO” si el promedio es mayor o igual a 7 o 
 “DESAPROBADO” si es menor.
 '''
-# def calcularAprobacion(primerNota= 1, segundaNota= 1, tercerNota= 1):
-#     promedio = (primerNota + segundaNota + tercerNota) / 3
+def calcularAprobacion(primerNota= 1, segundaNota= 1, tercerNota= 1):
+    promedio = (primerNota + segundaNota + tercerNota) / 3
 
-#     if(promedio >= 7):
-#         return print('APROBADO')
-#     else:
-#         return print('DESAPROBADO')
+    if(promedio >= 7):
+        return print('APROBADO')
+    else:
+      return print('DESAPROBADO')
     
-# calcularAprobacion(1,10,10)
+calcularAprobacion(1,10,10)
 
 '''
 Ejercicio 8
@@ -106,24 +106,25 @@ Escribir una función que recibe dos números enteros mayores que 1, n y m, y
 retorna la potencia n a la m. Resolver con una estructura de control repetitiva, 
 NO con el operador potencia de Python
 '''
-# def calcularPotencia(base= 1, exponente= 0):
-#     resultado = 1
-#     for i in range (exponente):
-#         resultado *= base
+def calcularPotencia(base= 1, exponente= 0):
+    resultado = 1
+    for i in range (exponente):
+      resultado *= base
 
-#     return resultado
+    return resultado
 
-# print(calcularPotencia(7,2))
+print(calcularPotencia(7,2))
 
 '''
 Ejercicio 9
 Escribir una función que dado un tiempo en horas, minutos y segundos retorne 
 esa misma cantidad en segundos.
 '''
-# def calcularTiempoEnSegundos(horas= 0, minutos= 0, segundos= 0):
-#     return (horas*3600) + (minutos*60) + segundos
+def calcularTiempoEnSegundos(horas= 0, minutos= 0, segundos= 0):
+    return (horas*3600) + (minutos*60) + segundos
 
-# print(calcularTiempoEnSegundos(1,1,40))
+print(calcularTiempoEnSegundos(1,1,40))
+
 '''
 Ejercicio 10
 Escribir una función que dado un año, retorne verdadero si es bisiesto y 
@@ -137,21 +138,21 @@ no es bisiesto
 def esAnioBisiesto(anio):
     return (anio % 4 == 0 and not anio % 100 == 0)
 
-# print(esAnioBisiesto(2020))
+print(esAnioBisiesto(2020))
 '''
 Ejercicio 11
 Escribir un programa que pida dos años y escriba cuántos años bisiestos hay entre 
 esas dos fechas (incluidos los dos años).
 '''
-# def cantidadDeAniosBisiestosEntre(anioInicio, anioFin):
-#     cantidad = 0
-#     for i in range (anioInicio + 1, anioFin):
-#         if(esAnioBisiesto(i)):
-#             cantidad += 1
+def cantidadDeAniosBisiestosEntre(anioInicio, anioFin):
+    cantidad = 0
+    for i in range (anioInicio + 1, anioFin):
+        if(esAnioBisiesto(i)):
+           cantidad += 1
 
-#     return cantidad
+    return cantidad
 
-# print(cantidadDeAniosBisiestosEntre(2019,2025))
+print(cantidadDeAniosBisiestosEntre(2019,2025))
 
 '''
 Ejercicio 12
@@ -160,18 +161,18 @@ de sus dígitos. Para obtener los digitos deben ir dividiendo (division entera)
 por 10 sucesivamente y tomando el resto (digito que esta mas a la derecha).
 '''
 
-# def sumarDigitos(numero):
+def sumarDigitos(numero):
 
-#     cantidad = 0
-#     longitud = len(str(numero))
+    cantidad = 0
+    longitud = len(str(numero))
 
-#     for i in range (longitud):
-#         cantidad += numero % 10
-#         numero = numero // 10
+    for i in range (longitud):
+        cantidad += numero % 10
+        numero = numero // 10
 
-#     return cantidad
+    return cantidad
 
-# print(sumarDigitos(127))
+print(sumarDigitos(127))
 
 '''
 Ejercicio 13
@@ -181,7 +182,7 @@ Mostrar el número cuya sumatoria de dígitos fue mayor y la cantidad de número
 sumatoria de dígitos fue menor que 10. Utilizar una o más funciones, 
 según sea necesario.
 '''
-[ ]
+
 '''
 Ejercicio 14
 Escribir un programa que solicita el ingreso de números primos. 
