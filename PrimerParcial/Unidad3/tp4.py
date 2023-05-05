f7502f8e6ffccd395301cd58ef75984a45eefe43
Kevin Caria <kcaria@pigmalion.com.ar>
2023-05-05 19:31:15 -0300

    update tp4

--- PrimerParcial/Unidad3/tp4.py
+++ PrimerParcial/Unidad3/tp4.py
@@ -106,11 +106,17 @@ def sumaVector(vector, indice):
 
     return suma
 
+def sumaVectorProfe(vector):
+    resultado = vector[0] #Caso base de la recursion sin if
+    if len(vector)>0:
+        resultado = resultado + sumaVectorProfe(vector[1:])
+    return resultado
+
 # Ejercicio 7
 # Escribir un programa que crea y cargar dos matrices de tamaño 3x3, las suma 
 # y muestra el resultado.
 primerMatriz = np.array([[1 , 2 , 3] ,[2 , 5 , 0],[3 , 0 , 9]])
-segundaMatriz = np.array([[1 , 2 , 2] ,[1 , 1 , 2], [1 , 1 , 1]])
+segundaMatriz = np.array([[1 , 0 , 0] ,[0 , 1 , 0], [0 , 0 , 1]])
 def sumarMatriz(primerMatriz, segundaMatriz):
     matrizSumada = np.zeros((3,3),int)
     for i in range(len(primerMatriz)):
@@ -121,12 +127,12 @@ def sumarMatriz(primerMatriz, segundaMatriz):
 # Ejercicio 8
 # Escribir una función que recibe una matriz y la rellena de la siguiente forma: 
 # En la posición M[ i , j ] debe contener i + j.
-def sumarPosiciones():
-    matrizSumada = np.zeros((3,3),int)
-    for i in range(len(matrizSumada)):
-        for j in range(len(matrizSumada[i])):
-            matrizSumada[i][j] = i + j
-    return matrizSumada
+matrizARellenar = np.zeros((3,3),int)
+
+def sumarPosiciones(matrizARellenar):
+    for i in range(len(matrizARellenar)):
+        for j in range(len(matrizARellenar[i])):
+            matrizARellenar[i][j] = i + j
 
 # Ejercicio 9
 # Desarrollar una función que recibe una matriz cuadrada de números reales (N x N) 
@@ -135,7 +141,7 @@ def sumarPosiciones():
 
 # En el ejemplo de la imagen, la función deberia retornar: b + c + f
 
-def ejercicio9(matriz):
+def sumaSobreDiagonal(matriz):
     suma = 0
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
@@ -152,14 +158,11 @@ def ejercicio9(matriz):
 # menos en la diagonal principal.
 
 def esMatrizDiagonal(matriz):
-    resultado = False
+    resultado = True
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
-            if (i == j and matriz[i,j] != 0) or (i != j and matriz[i,j] == 0) :
-                    resultado = True
-            else: 
-                resultado = False
-                break
+            if not((i == j and matriz[i,j] != 0) or (i != j and matriz[i,j] == 0)) :
+                    resultado = False
     
     return resultado
 
@@ -172,19 +175,15 @@ def esMatrizDiagonal(matriz):
 
 # texto alternativo
 
-def ejercicio11(matriz):
-    resultado = False
+def esSimetrica(matriz):
+    resultado = True
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
             if i != j:
-                if matriz[i,j] == matriz[j,i]:
-                    resultado = True
-                else: 
+                if matriz[i,j] != matriz[j,i]:
                     resultado = False
-                    break
     return resultado
 
-print(ejercicio11(primerMatriz))
 # Ejercicio 12
 # Escribir una función recursiva, que calcule la suma de los elementos 
 # de la diagonal principal de una matriz.
@@ -192,11 +191,18 @@ print(ejercicio11(primerMatriz))
 # Nota: La suma de los elementos de la diagonal principal de una matriz 
 # se llama "traza de la matriz".
 
-
 # Comentario
 # En los ejercicios que usan cadenas de caracteres, pueden usar el tipo str, 
 # salvo que necesiten modificar elementos de la cadena luego de crearla, en ese caso, 
 # tienen que usar arreglos de caracteres.
+def sumarDiagonal(matriz, indice):
+    resultado = 0
+    if indice == 0:
+        resultado += matriz[indice, indice] 
+    else:
+        resultado += matriz[indice, indice] + sumarDiagonal(matriz, indice-1)
+
+    return resultado
 
 # Ejercicio 13
 # Escribir 3 procedimientos que reciben una cadena de caracteres y..:
@@ -204,14 +210,38 @@ print(ejercicio11(primerMatriz))
 # Imprima los dos primeros caracteres.
 # Imprima los tres últimos caracteres.
 # Imprima la cadena cada dos caracteres. Por ej.: 'recta' debería imprimir 'rca'
+arreglo = np.array(['p', 'y', 't', 'h', 'o', 'n'])
+
+arreglo2 = np.array(['p', 't', 't', 't', 't', 'p'])
 
 
+def imprimirDosPrimeros(arreglo):
+    return print(arreglo[0]+arreglo[1])
+
+def imprimirTresUltimos(arreglo):
+    return print(arreglo[-3]+arreglo[-2]+arreglo[-1])
+
+def imprimirCadaDos(arreglo):
+    resultado = ''
+    for indice in range(0,len(arreglo),2):
+        resultado += arreglo[indice]
+    return print(resultado)
+
+imprimirCadaDos(arreglo)
 # Ejercicio 14
 # Desarrollar una función recursiva que retorna True si una cadena de caracteres 
 # es un palíndromo y False en caso contrario.
 
 # Nota: Palíndromo es la forma elegante de "capicua"
 
+def esPalindromo(arreglo, inicio, final):
+    resultado = True
+    if inicio >= final:
+        resultado = True
+    else:
+        resultado = arreglo[inicio] == arreglo[final] and esPalindromo(arreglo, inicio + 1, final -1)
+
+    return resultado
 
 # Ejercicio 15
 # Escribir una función que recibe una cadena de caracteres y un caracter y 
@@ -220,6 +250,25 @@ print(ejercicio11(primerMatriz))
 # Por ej si la cadena es: ' separar ' y el caracter es: ' , ', 
 # debería retornar ' s,e,p,a,r,a,r '
 
+def arregloConCaracterNuevo(arreglo,caracter):
+    arregloNuevo = np.empty((len(arreglo)*2,), dtype=str)
+
+    for i in range(len(arreglo)):
+        arregloNuevo[i*2] = arreglo[i]
+        arregloNuevo[i*2+1] = caracter
+
+    return arregloNuevo
+
+def arregloConCaracter(arreglo,caracter):
+    arregloNuevo = np.empty((len(arreglo)*2-1,), dtype=str)
+
+    for i in range(len(arregloNuevo)):
+        if i%2==0:
+            arregloNuevo[i]=arreglo[i//2]
+        else:
+            arregloNuevo[i] = caracter
+
+    return arregloNuevo
 
 # Ejercicio 16
 # Escribir una función que recibe un arreglo variables de tipo "Tiempo" 
@@ -230,3 +279,23 @@ print(ejercicio11(primerMatriz))
 
 # Nota: Van a tener que copiar el TDA "Tiempo" en el bloque de código de abajo 
 # y para que un print de un vector de variables de tipo "Tiempo" funcione, tienen que programar la operacion "__repr__()" en el TDA "Tiempo".
+
+
+# Ejercicio extra
+# hacer una funcion recursiva que reciba un arreglo y retorne el minimo
+def minimoVector(vector, indice):
+    if indice == 0:
+        return vector[indice]
+    else:
+        valor_actual = vector[indice]
+        valor_minimo_anterior = minimoVector(vector, indice - 1)
+        if valor_actual < valor_minimo_anterior:
+            return valor_actual
+        else:
+            return valor_minimo_anterior
+
+def minimoVectorProfe(vector):
+    if len(vector) == 1:
+        return vector[0]
+    else:
+        return min(vector[0], minimoVectorProfe(vector[1:])) #Usando funcion min y [1:]
